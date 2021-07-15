6dad6d99bfc76544ceeac5c76e2df82641ee5ca8
Jon Cram <webignition@gmail.com>
2021-07-15 15:10:01 +0100

    Import model and factory

---
+++ src/Configuration.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace SmartAssert\DigitalOceanDropletConfiguration;
+
+class Configuration
+{
+    /**
+     * @param string[]    $names
+     * @param string      $region
+     * @param string      $size
+     * @param string      $image
+     * @param bool        $backups
+     * @param bool        $ipv6
+     * @param bool|string $vpcUuid
+     * @param int[]       $sshKeys
+     * @param string      $userData
+     * @param bool        $monitoring
+     * @param string[]    $volumes
+     * @param string[]    $tags
+     */
+    public function __construct(
+        private array $names,
+        private string $region,
+        private string $size,
+        private string $image,
+        private bool $backups,
+        private bool $ipv6,
+        private string | bool $vpcUuid,
+        private array $sshKeys,
+        private string $userData,
+        private bool $monitoring,
+        private array $volumes,
+        private array $tags,
+    ) {
+        $this->setNames($names);
+        $this->setSshKeys($sshKeys);
+        $this->setVolumes($volumes);
+        $this->setTags($tags);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNames(): array
+    {
+        return $this->names;
+    }
+
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    public function getSize(): string
+    {
+        return $this->size;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getBackups(): bool
+    {
+        return $this->backups;
+    }
+
+    public function getIpv6(): bool
+    {
+        return $this->ipv6;
+    }
+
+    public function getVpcUuid(): string | bool
+    {
+        return $this->vpcUuid;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function getSshKeys(): array
+    {
+        return $this->sshKeys;
+    }
+
+    public function getUserData(): string
+    {
+        return $this->userData;
+    }
+
+    public function getMonitoring(): bool
+    {
+        return $this->monitoring;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getVolumes(): array
+    {
+        return $this->volumes;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string[] $names
+     */
+    public function withNames(array $names): self
+    {
+        $new = clone $this;
+        $new->setNames($names);
+
+        return $new;
+    }
+
+    public function withRegion(string $region): self
+    {
+        $new = clone $this;
+        $new->region = $region;
+
+        return $new;
+    }
+
+    public function withSize(string $size): self
+    {
+        $new = clone $this;
+        $new->size = $size;
+
+        return $new;
+    }
+
+    public function withImage(string $image): self
+    {
+        $new = clone $this;
+        $new->image = $image;
+
+        return $new;
+    }
+
+    public function withBackups(bool $backups): self
+    {
+        $new = clone $this;
+        $new->backups = $backups;
+
+        return $new;
+    }
+
+    public function withIpv6(bool $ipv6): self
+    {
+        $new = clone $this;
+        $new->ipv6 = $ipv6;
+
+        return $new;
+    }
+
+    public function withVpcUuid(string | bool $vpcUuid): self
+    {
+        $new = clone $this;
+        $new->vpcUuid = $vpcUuid;
+
+        return $new;
+    }
+
+    /**
+     * @param int[] $sshKeys
+     */
+    public function withSshKeys(array $sshKeys): self
+    {
+        $new = clone $this;
+        $new->setSshKeys($sshKeys);
+
+        return $new;
+    }
+
+    public function withUserData(string $userData): self
+    {
+        $new = clone $this;
+        $new->userData = $userData;
+
+        return $new;
+    }
+
+    public function withMonitoring(bool $monitoring): self
+    {
+        $new = clone $this;
+        $new->monitoring = $monitoring;
+
+        return $new;
+    }
+
+    /**
+     * @param string[] $volumes
+     */
+    public function withVolumes(array $volumes): self
+    {
+        $new = clone $this;
+        $new->setVolumes($volumes);
+
+        return $new;
+    }
+
+    /**
+     * @param string[] $tags
+     */
+    public function withTags(array $tags): self
+    {
+        $new = clone $this;
+        $new->setTags($tags);
+
+        return $new;
+    }
+
+    /**
+     * @param string[] $tags
+     */
+    public function addTags(array $tags): self
+    {
+        $new = clone $this;
+        $tags = array_merge($this->tags, $tags);
+
+        return $new->withTags($tags);
+    }
+
+    public function setBackups(bool $backups): self
+    {
+        $new = clone $this;
+        $new->backups = $backups;
+
+        return $new;
+    }
+
+    /**
+     * @param int[] $sshKeys
+     */
+    public function setSshKeys(array $sshKeys): void
+    {
+        $sshKeys = array_filter($sshKeys, function ($item) {
+            return is_int($item);
+        });
+
+        $this->sshKeys = array_unique($sshKeys);
+    }
+
+    /**
+     * @param string[] $volumes
+     */
+    public function setVolumes(array $volumes): void
+    {
+        $volumes = array_filter($volumes, function ($item) {
+            return is_string($item);
+        });
+
+        $this->volumes = array_unique($volumes);
+    }
+
+    /**
+     * @param string[] $tags
+     */
+    public function setTags(array $tags): void
+    {
+        $tags = array_filter($tags, function ($item) {
+            return is_string($item);
+        });
+
+        $this->tags = array_unique($tags);
+    }
+
+    /**
+     * @param string[] $names
+     */
+    private function setNames(array $names): void
+    {
+        $names = array_filter($names, function ($item) {
+            return is_string($item);
+        });
+
+        $this->names = array_unique($names);
+    }
+}
---
+++ src/Factory.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace SmartAssert\DigitalOceanDropletConfiguration;
+
+class Factory
+{
+    public const KEY_NAMES = 'names';
+    public const KEY_REGION = 'region';
+    public const KEY_SIZE = 'size';
+    public const KEY_IMAGE = 'image';
+    public const KEY_BACKUPS = 'backups';
+    public const KEY_IPV6 = 'ipv6';
+    public const KEY_VPC_UUID = 'vpc-uuid';
+    public const KEY_SSH_KEYS = 'ssk-keys';
+    public const KEY_USER_DATA = 'user-data';
+    public const KEY_MONITORING = 'monitoring';
+    public const KEY_VOLUMES = 'volumes';
+    public const KEY_TAGS = 'tags';
+
+    private const DEFAULT_NAMES = [];
+    private const DEFAULT_REGION = '';
+    private const DEFAULT_SIZE = '';
+    private const DEFAULT_IMAGE = '';
+    private const DEFAULT_BACKUPS = false;
+    private const DEFAULT_IPV6 = false;
+    private const DEFAULT_VPC_UUID = false;
+    private const DEFAULT_SSH_KEYS = [];
+    private const DEFAULT_USER_DATA = '';
+    private const DEFAULT_MONITORING = true;
+    private const DEFAULT_VOLUMES = [];
+    private const DEFAULT_TAGS = [];
+
+    /**
+     * @param array<mixed> $defaults
+     */
+    public function __construct(
+        private array $defaults = [
+            self::KEY_NAMES => self::DEFAULT_NAMES,
+            self::KEY_REGION => self::DEFAULT_REGION,
+            self::KEY_SIZE => self::DEFAULT_SIZE,
+            self::KEY_IMAGE => self::DEFAULT_IMAGE,
+            self::KEY_BACKUPS => self::DEFAULT_BACKUPS,
+            self::KEY_IPV6 => self::DEFAULT_IPV6,
+            self::KEY_VPC_UUID => self::DEFAULT_VPC_UUID,
+            self::KEY_SSH_KEYS => self::DEFAULT_SSH_KEYS,
+            self::KEY_USER_DATA => self::DEFAULT_USER_DATA,
+            self::KEY_MONITORING => self::DEFAULT_MONITORING,
+            self::KEY_VOLUMES => self::DEFAULT_VOLUMES,
+            self::KEY_TAGS => self::DEFAULT_TAGS,
+        ]
+    ) {
+    }
+
+    public function create(): Configuration
+    {
+        return new Configuration(
+            $this->getStringValues(self::KEY_NAMES),
+            $this->getStringValue(self::KEY_REGION),
+            $this->getStringValue(self::KEY_SIZE),
+            $this->getStringValue(self::KEY_IMAGE),
+            $this->getBooleanValue(self::KEY_BACKUPS),
+            $this->getBooleanValue(self::KEY_IPV6),
+            $this->getVpcUuidValue(),
+            $this->getSshKeyValues(),
+            $this->getStringValue(self::KEY_USER_DATA),
+            $this->getBooleanValue(self::KEY_MONITORING, true),
+            $this->getStringValues(self::KEY_VOLUMES),
+            $this->getStringValues(self::KEY_TAGS),
+        );
+    }
+
+    private function getVpcUuidValue(): string | bool
+    {
+        $value = $this->defaults[self::KEY_VPC_UUID] ?? false;
+
+        if (is_bool($value)) {
+            return $value;
+        }
+
+        if (is_string($value)) {
+            return $value;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getStringValues(string $key): array
+    {
+        $values = $this->getValues($key);
+
+        return array_filter($values, function ($item) {
+            return is_string($item);
+        });
+    }
+
+    /**
+     * @return int[]
+     */
+    private function getSshKeyValues(): array
+    {
+        $values = $this->getValues(self::KEY_SSH_KEYS);
+
+        return array_filter($values, function ($item) {
+            return is_int($item);
+        });
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    private function getValues(string $key): array
+    {
+        $values = $this->defaults[$key] ?? [];
+
+        return is_array($values) ? $values : [];
+    }
+
+    private function getStringValue(string $key): string
+    {
+        $value = $this->defaults[$key] ?? '';
+
+        if (is_string($value)) {
+            return $value;
+        }
+
+        if (is_scalar($value)) {
+            return (string) $value;
+        }
+
+        return '';
+    }
+
+    private function getBooleanValue(string $key, bool $default = false): bool
+    {
+        $value = $this->defaults[$key] ?? '';
+
+        if (is_bool($value)) {
+            return $value;
+        }
+
+        if (is_scalar($value)) {
+            return (bool) $value;
+        }
+
+        return $default;
+    }
+}
